a4ba1ebdd230b6f131d6a394bad5b7a589b7f324
bnagle22 <naglebenjamin@gmail.com>
2021-10-04 16:15:24 -0400

    Add getGuess, play, and render methods

--- app.js
+++ app.js
@@ -7,22 +7,30 @@ const game = {
   getGuess: function() {
     let guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} 
     and ${this.biggestNum}.`))
-    console.log(guess)
     return guess
-  },
+    },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     while(this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum){
-      this.prevGuesses.push(this.getGuess())
-      this.render()
+      let guess = this.getGuess()
+      if (guess >= this.smallestNum && 
+        guess <= this.biggestNum){
+        this.prevGuesses.push(guess)
+        console.log(this.prevGuesses)
+        this.render()
+    } else{
+      alert("Error, input must be a number in the specified range.")
+    }
     }
   },
   render: function(){
     if (this.prevGuesses[this.prevGuesses.length -1] > this.secretNum){
       alert(`Your guess is too high. Previous guesses: ${this.prevGuesses}`)
+      this.biggestNum = this.prevGuesses[this.prevGuesses.length -1]
     } else if (this.prevGuesses[this.prevGuesses.length -1] < this.secretNum){
       alert(`Your guess is too low. Previous guesses: ${this.prevGuesses}`)
+      this.smallestNum = this.prevGuesses[this.prevGuesses.length -1]
     } else {
       alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses.`)
     }
